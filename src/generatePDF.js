05b63f898218f900f5402bad393b492636c3bc0b
Seva Zaikov <seva.zaikov@gmail.com>
2018-10-28 13:57:14 -0700

    add basic styling inside PDF

--- src/generatePDF.js
+++ src/generatePDF.js
@@ -13,7 +13,27 @@ var fontDescriptors = {
 const printer = new pdfMakePrinter(fontDescriptors);
 
 module.exports.generatePDF = ({ res, allergies, lang }) => {
+  const date = new Date().toDateString();
   const docDefinition = {
+    info: {
+      title: "Food allergies",
+      author: "seva zaikov <seva.zaikov@gmail.com>",
+      subject: "Food allergies list",
+      keywords: "food allergies, allergens",
+      creator: "seva zaikov <seva.zaikov@gmail.com>",
+      producer: "seva zaikov <seva.zaikov@gmail.com>",
+      creationDate: date,
+      modDate: date,
+      language: lang
+    },
+    header: {
+      text: "https://allergy-checker.2018.nodeknockout.com",
+      alignment: "center",
+      margin: [0, 20, 0, 20],
+      decoration: "underline",
+      color: "#3490dc"
+    },
+    footer: renderFooter(),
     content: [
       ...renderHeader(lang),
       ...renderAllergies({ allergies, lang }).reduce(
@@ -31,87 +51,139 @@ module.exports.generatePDF = ({ res, allergies, lang }) => {
 const EXPLANATIONS = {
   milk: {
     en: [
-      "No dairy products",
-      "products like milk, cheese, butter, ice cream, cream, yogurt, margarine"
+      makeHeader("No dairy products"),
+      makeDescription(
+        "Products like milk, cheese, butter, ice cream, cream, yogurt, margarine"
+      )
     ],
     ru: [
-      "Без молочных продуктов",
-      "Такие продукты, как молоко, сыр, масло, мороженое, сливки, йогурт, маргарин"
+      makeHeader("Без молочных продуктов"),
+      makeDescription(
+        "Такие продукты, как молоко, сыр, масло, мороженое, сливки, йогурт, маргарин"
+      )
     ]
   },
   eggs: {
-    en: ["No eggs", "No white eggs or yolks"],
-    ru: ["Без яиц", "Без белка и желтка"]
+    en: [makeHeader("No eggs"), makeDescription("No white eggs or yolks")],
+    ru: [makeHeader("Без яиц"), makeDescription("Без белка и желтка")]
   },
   nuts: {
     en: [
-      "No nuts",
-      "No peanuts, almonds, cashews, walnuts, pistachios, brazil nuts, macadamia nuts, pine nuts"
+      makeHeader("No nuts"),
+      makeDescription(
+        "No peanuts, almonds, cashews, walnuts, pistachios, brazil nuts, macadamia nuts, pine nuts"
+      )
     ],
     ru: [
-      "Без орехов",
-      "Арахис, миндаль, кешью, грецкие орехи, фисташки, бразильский орех, орехи макадамии, кедровые орешки"
+      makeHeader("Без орехов"),
+      makeDescription(
+        "Арахис, миндаль, кешью, грецкие орехи, фисташки, бразильский орех, орехи макадамии, кедровые орешки"
+      )
     ]
   },
   shellfish: {
     en: [
-      "No shellfish",
-      "No products like shrimp, prawns, crayfish, lobster, squid, scallops"
+      makeHeader("No shellfish"),
+      makeDescription(
+        "No products like shrimp, prawns, crayfish, lobster, squid, scallops"
+      )
     ],
     ru: [
-      "Без моллюсков",
-      "Такие продукты, как креветки, пильчатые креветки, раки, лобстеры, кальмары и морские гребешки"
+      makeHeader("Без моллюсков"),
+      makeDescription(
+        "Такие продукты, как креветки, пильчатые креветки, раки, лобстеры, кальмары и морские гребешки"
+      )
     ]
   },
   gluten: {
     en: [
-      "No gluten or wheat",
-      "Wheat contains gluten, so no wheat please, and other products with gluten"
+      makeHeader("No gluten or wheat"),
+      makeDescription(
+        "Wheat contains gluten, so no wheat please, and other products with gluten"
+      )
     ],
     ru: [
-      "Без глютена и пшеницы",
-      "Пшеница содержит глютен, поэтому без пшеницы, пожалуйста, и других продуктов с глютеном"
+      makeHeader("Без глютена и пшеницы"),
+      makeDescription(
+        "Пшеница содержит глютен, поэтому без пшеницы, пожалуйста, и других продуктов с глютеном"
+      )
     ]
   },
   soy: {
     en: [
-      "No soy",
-      "No products like soybeans and soy products like soy milk or soy sauce"
+      makeHeader("No soy"),
+      makeDescription(
+        "No products like soybeans and soy products like soy milk or soy sauce"
+      )
     ],
     ru: [
-      "Без сои",
-      "Такие продукты, как соевое молоко, соевый соус или соевые бобы"
+      makeHeader("Без сои"),
+      makeDescription(
+        "Такие продукты, как соевое молоко, соевый соус или соевые бобы"
+      )
     ]
   },
   fish: {
     en: [
-      "No fish",
-      "No fish with fins – please note that it is different from shellfish!"
+      makeHeader("No fish"),
+      makeDescription(
+        "No fish with fins – please note that it is different from shellfish!"
+      )
     ],
     ru: [
-      "Без рыбы",
-      "Без рыбы с плавниками – заметьте, моллюски не входят в эту категорию"
+      makeHeader("Без рыбы"),
+      makeDescription(
+        "Без рыбы с плавниками – заметьте, моллюски не входят в эту категорию"
+      )
     ]
   }
 };
 
+function makeHeader(text) {
+  return { text, fontSize: 18, bold: true, margin: [0, 0, 0, 5] };
+}
+
+function makeDescription(text) {
+  return { text, margin: [0, 0, 0, 10] };
+}
+
 function renderHeader(lang) {
+  const greetings = text => ({
+    text,
+    fontSize: 18,
+    bold: true,
+    margin: [0, 20, 0, 10]
+  });
+  const lastLine = text => ({ text, margin: [0, 0, 0, 20] });
   if (lang === "ru") {
     return [
-      "Здравствуйте!",
+      greetings("Здравствуйте!"),
       "У меня есть аллергия на некоторые виды еды, поэтому, пожалуйста, проверьте, что их нет в моём заказе.",
       "Это очень важно для моего здоровья – я не хочу портить своё время плохим самочувствием или походом к врачу.",
-      "Большое спасибо!"
+      lastLine("Большое спасибо!")
     ];
   }
 
   // we render english version if we don't have info for this lang.
   return [
-    "Hello!",
+    greetings("Hello!"),
     "I have food allergies, please make sure that products I order don't contain them.",
-    "It is very important for my health – I don't want to ruin my time with not well-being or visiting a doctor.",
-    "Thanks a lot!"
+    "It is very important for my health – I don't want to ruin my time not feeling good or visiting a doctor.",
+    lastLine("Thanks a lot!")
   ];
+}
+
+function renderFooter() {
+  return {
+    columns: [
+      { text: "Food allergy list", margin: [40, 0, 0, 0] },
+      {
+        text: "seva.zaikov@gmail.com",
+        alignment: "right",
+        margin: [0, 0, 40, 0]
+      }
+    ]
+  };
 }
 
 function renderAllergies({ allergies, lang }) {
